242dcf3e3c22460aed83c357c3f16a04f8608c66
anshumancodes <anshumanprof01@gmail.com>
2026-06-29 03:08:41 +0530

    feat(caption): add srt parsing and text saving

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import argparse
-from downloader import get_video_info , get_video_caption_url , download_caption
-
+from downloader import get_video_info, get_video_caption_url, download_caption
+from formatter import parse_srt, save_text
 from pprint import pprint
 
 
@@ -33,12 +33,23 @@ def main():
     # getting video info from downloader get_video_info fucntion
     video = get_video_info(url=args.url)
 
-   #getting captions url
-    caption_url=get_video_caption_url(video__info=video)
+   # getting captions url
+    caption_url = get_video_caption_url(video__info=video)
+
+   # downloading the caption from the caption url (in srt format)
     if not caption_url:
         print("no caption available")
-    print("caption url :  ",caption_url)
-    caption=download_caption(caption_url=caption_url,output_dir=args.output)
+    print("caption url :  ", caption_url)
+    srt_output_path = download_caption(
+        caption_url=caption_url, output_dir=args.output)
+
+    # then i will pass it to srt parser
+    # to parse the srt we are saving
+    segments = parse_srt(srt_output_path)
+
+    # saving parsed srt segements's text in transcripts.txt
+    transcript_txt = save_text(segments=segments, output_dir=arg.output)
+
 
 if __name__ == "__main__":
     main()
