71012510cd78031bd6316e24b27f6979a98b6836
anshumancodes <anshumanprof01@gmail.com>
2026-06-29 04:12:42 +0530

    feat(downloader): add video download functionality

--- downloader.py
+++ downloader.py
@@ -39,7 +39,7 @@ def get_video_caption_url(video__info, language="en"):
 
 
 def download_caption(caption_url, output_dir):
-    # hitting the url 
+    # hitting the url
     response = httpx.get(caption_url)
 
     print(response.status_code)
@@ -49,5 +49,22 @@ def download_caption(caption_url, output_dir):
     with open(output_path, "w", encoding="utf-8") as f:
 
         f.write(response.text)
-        print(f"downloading caption is completed sucessfully!, see the file at {output_path}")
+        print(
+            f"downloading caption is completed sucessfully!, see the file at {output_path}")
         return output_path
+
+
+# we will download the video if no captions are avialble
+
+def download_video(url, output_dir):
+    options = {
+        "quiet": True,
+        "skip_download": False,
+        "outtmpl": str(Path(output_dir) / "video.%(ext)s"),
+    }
+    with YoutubeDL(options) as ydl:
+        ydl.download([url])
+        video_info = ydl.extract_info(url=url, download=True)
+        video_path = ydl.prepare_filename(video_info)
+        
+    return video_path
