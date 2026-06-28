cb95f3e4bad6db684ef961cf2eb8ee8a07b015dc
anshumancodes <anshumanprof01@gmail.com>
2026-06-28 14:53:35 +0530

    feat(cli): add youtube url and output directory args

---
+++ main.py
@@ -0,0 +1,30 @@
+import argparse
+
+def parse_args():
+    parser=argparse.ArgumentParser(
+        description="Generate transcripts from youtube videos"
+    )
+    # url is mandatory , because without it we cant do anytbing really
+    parser.add_argument(
+        "--url",
+        required=True,
+        help="Yt video Url"
+    )
+    #output dir argurement 
+    parser.add_argument(
+        "--output",
+        default="outputs",
+        help="output directory , default directory is set to `outputs`"
+    )
+    return parser.parse_args()
+
+def main():
+    args=parse_args()
+
+    print("youtube transriber")
+    print ("url: ",args.url)
+    print(" output dir :",args.output)
+
+
+if __name__== "__main__":
+    main()
