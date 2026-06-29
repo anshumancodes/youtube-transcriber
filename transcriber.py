e9906aacbdd4ba3629b626495e1581f96c5e7846
anshumancodes <anshumanprof01@gmail.com>
2026-06-29 13:33:36 +0530

    feat(transcriber): add basic audio transcription

---
+++ transcriber.py
@@ -0,0 +1,25 @@
+from faster_whisper import WhisperModel
+model = WhisperModel(
+    "base",
+    device="cpu",
+    compute_type="int8",
+)
+
+
+def transcribe_audio(audio_path):
+    whisper_segments, info = model.transcribe(audio_path, vad_filter=True)
+
+    print("Detected language '%s' with probability %f" %
+          (info.language, info.language_probability))
+
+    # storing parsed sgement from whisper's sent segments here
+    parsed_segments = []
+
+    for segment in whisper_segment:
+        parsed_segments.append({
+            "start": segment.start,
+            "end": segment.end,
+            "text": segment.text.strip()
+        })
+
+    return parsed_segments
